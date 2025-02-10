331a90796e89e9a35d395b874ca602af30c9671c
Krishna Amuluru <krishna.amuluru@gmail.com>
2025-02-09 21:04:32 -0500

    transition animations

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,65 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Fade Animation Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const MyHomePage(title: 'CW1'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
 
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  bool _showFirstImage = true; // Controls which image to display
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  bool _showFirstImage = true;
 
-  // Increments the counter by 1.
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  late AnimationController _animationController;
+  late Animation<double> _fadeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 800),
+    );
+    _fadeAnimation = CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeInOut,
+    );
+
+    // Start fully visible
+    _animationController.forward();
   }
 
-  // Toggles the image between two different assets.
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   void _toggleImage() {
     setState(() {
       _showFirstImage = !_showFirstImage;
     });
+    _animationController.reset();
+    _animationController.forward();
   }
 
   @override
@@ -50,44 +67,30 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-            ),
-            SizedBox(height: 20),
-            // Display the image based on _showFirstImage
-            Image.asset(
-              _showFirstImage ? 'assets/landscape.jpeg' : 'assets/ocean.jpg',
-              height: 200,
-            ),
-          ],
-        ),
-      ),
-      // Two FloatingActionButtons: one for incrementing and one for switching images.
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: <Widget>[
-          FloatingActionButton(
-            onPressed: _incrementCounter,
-            tooltip: 'Increment',
-            child: Icon(Icons.add),
-          ),
-          SizedBox(height: 16),
-          FloatingActionButton(
+        actions: [
+          ElevatedButton(
             onPressed: _toggleImage,
-            tooltip: 'Switch Image',
-            child: Icon(Icons.image),
+            child: Text("Toggle Image"),
+            
           ),
         ],
+      ),
+      body: Center(
+        child: FadeTransition(
+          opacity: _fadeAnimation,
+          child: Image.asset(
+            _showFirstImage
+                ? 'assets/landscape.jpeg'
+                : 'assets/ocean.jpg',
+            width: 150,
+            height: 150,
+          ),
+        ),
       ),
     );
   }
 }
+
 
 
 
